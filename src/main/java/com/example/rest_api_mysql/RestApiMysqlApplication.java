e8599d6b5c4808b05ad65fa2ed8dad0bf9e5cff4
1599982 <1599982@senati.pe>
2025-03-10 09:42:25 -0500

    Init commit

---
+++ src/main/java/com/example/rest_api_mysql/RestApiMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.rest_api_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMysqlApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiMysqlApplication.class, args);
+		System.out.print("OK!!!!!!!!!!!!!!!!!!!!!!");
+	}
+}
