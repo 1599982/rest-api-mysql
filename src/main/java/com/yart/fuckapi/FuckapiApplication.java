689beba5acf01a85f4c464c4fca9bcc753557fe3
1599982 <1599982@senati.pe>
2025-11-23 00:34:16 -0500

    Init commit

---
+++ src/main/java/com/yart/fuckapi/FuckapiApplication.java
@@ -0,0 +1,13 @@
+package com.yart.fuckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuckapiApplication.class, args);
+	}
+
+}
